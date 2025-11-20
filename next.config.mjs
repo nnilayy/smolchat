83a0bfee1d7f693a32deead4dcb16a005ce2316d
nnilayy <nnilayy.work@gmail.com>
2025-11-21 01:23:17 +0530

    chore(config): add project configuration files for Next.js, Tailwind, TypeScript, and shadcn/ui

---
+++ next.config.mjs
@@ -0,0 +1,34 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["faiss-node"],
+  },
+  async rewrites() {
+    return [
+      // Wildcard path matching
+      {
+        source: "/",
+        destination: "/chat/new",
+      },
+    ];
+  },
+  //   async rewrites() {
+  //     return [
+  //       {
+  //         source: "/api/anthropic/:path*",
+  //         destination: "https://api.anthropic.com/v1/messages",
+  //         // Proxy to Backend
+  //       },
+  //     ];
+  //   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "*",
+      },
+    ],
+  },
+};
+
+export default nextConfig;
